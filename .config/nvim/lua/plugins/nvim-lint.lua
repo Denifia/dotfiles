138c1ff47216a2b12daf872e37b3ab6ddae10d16
Denifia <mr.l.wale@gmail.com>
2025-04-26 16:30:41 +0800

    tweaking spell check settings
    I didn't want spell checking for all markdown

--- .config/nvim/lua/plugins/nvim-lint.lua
+++ .config/nvim/lua/plugins/nvim-lint.lua
@@ -1,10 +1,10 @@
 return {
-	-- https://github.com/mfussenegger/nvim-lint
-	-- Enable linting
-	"mfussenegger/nvim-lint",
-	config = function()
-		require("lint").linters_by_ft = {
-			markdown = { "alex", "write_good" },
-		}
-	end,
+  -- https://github.com/mfussenegger/nvim-lint
+  -- Enable linting
+  'mfussenegger/nvim-lint',
+  config = function()
+    require('lint').linters_by_ft = {
+      -- markdown = { "alex", "write_good" },
+    }
+  end,
 }
